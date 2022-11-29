95f7a5ed41a3d1521937eb4e97d1ac37201b2c22
SOUFIANEMEJAHED <soufiane.mejahed@epitech.eu>
2022-11-29 20:20:40 +0100

    creation class RangeSelectorTextField

--- lib/range_selector_page.dart
+++ lib/range_selector_page.dart
@@ -20,23 +20,13 @@ class _RangeSelectorPageState extends State<RangeSelectorPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              TextFormField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(), label: Text("Minimum")),
-                keyboardType: TextInputType.numberWithOptions(
-                  decimal: false,
-                  signed: true,
-                ),
+              RangeSelectorTextFormField(
+                labelText: 'Minimum',
               ),
               SizedBox(height: 12),
-              TextFormField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(), label: Text("Maximum")),
-                keyboardType: TextInputType.numberWithOptions(
-                  decimal: false,
-                  signed: true,
-                ),
-              )
+              RangeSelectorTextFormField(
+                labelText: 'Maximum',
+              ),
             ],
           ),
         ),
@@ -47,6 +37,26 @@ class _RangeSelectorPageState extends State<RangeSelectorPage> {
           // TODO :validate the Form
           // TODO : Navigate to the genetrator page
         },
+      ),
+    );
+  }
+}
+
+class RangeSelectorTextFormField extends StatelessWidget {
+  const RangeSelectorTextFormField({
+    Key? key,
+    required this.labelText,
+  }) : super(key: key);
+
+  final String labelText;
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration:
+          InputDecoration(border: OutlineInputBorder(), labelText: labelText),
+      keyboardType: TextInputType.numberWithOptions(
+        decimal: false,
+        signed: true,
       ),
     );
   }
